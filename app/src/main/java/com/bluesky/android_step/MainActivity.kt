fa2308893a5c8c34e610738e8ca92df6213b3792
BlueSky <dbcjdrb1997@gmail.com>
2024-01-21 23:21:02 +0900

    update Fragment LifeCycle Basic

--- app/src/main/java/com/bluesky/android_step/MainActivity.kt
+++ app/src/main/java/com/bluesky/android_step/MainActivity.kt
@@ -2,10 +2,33 @@ package com.bluesky.android_step
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.commit
+import com.bluesky.android_step.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
+
+        binding.run {
+            fragment1Btn.setOnClickListener {
+                setFragment(Fragment1())
+            }
+            fragment2Btn.setOnClickListener {
+                setFragment(Fragment2())
+            }
+        }
+
+    }
+    private fun setFragment(frag : Fragment) {  //2번
+        supportFragmentManager.commit {
+            replace(R.id.frameLayout, frag)
+            setReorderingAllowed(true)
+            addToBackStack("")
+        }
     }
 }
