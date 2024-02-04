cfc14ec45b9b327ec35eb6ca5e474a04442211be
BlueSky <dbcjdrb1997@gmail.com>
2024-02-04 23:27:01 +0900

    [Click-Close-Coordinate]  Find A coordinate and Calculate Compare

--- app/src/main/java/com/bluesky/android_step/MainActivity.kt
+++ app/src/main/java/com/bluesky/android_step/MainActivity.kt
@@ -2,38 +2,74 @@ package com.bluesky.android_step
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
 import android.util.Log
 import android.view.MotionEvent
 import android.widget.Button
 import android.widget.TextView
 import androidx.constraintlayout.widget.ConstraintLayout
+import kotlin.math.hypot
+import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
+    lateinit var button1: Button
+    lateinit var button2: Button
+    lateinit var textView: TextView
+    private val handler = Handler()
+    private var count = 0;
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         // Find the button and text view by their IDs
-        val button1= findViewById<Button>(R.id.button1)
-        val button2 = findViewById<Button>(R.id.button2)
-        val textView = findViewById<TextView>(R.id.textview)
+        button1= findViewById<Button>(R.id.button1)
+        button2 = findViewById<Button>(R.id.button2)
+        textView = findViewById<TextView>(R.id.textview)
         // x :  600 , y 1360
 
 
 //
 //        // Optionally, update the text view with the coordinates
         button1.setOnClickListener {
-            textView.text = "Touch event simulated on button1"
+            textView.text = "Button 1 clicked $count"
         }
+
         button2.setOnClickListener {
-            val location = IntArray(2)
-            button1.getLocationOnScreen(location)
-            val currentX = location[0]
-            val currentY = location[1]
-            simulateTouchEvent(currentX,currentY)
+            textView.text = "Button 2 clicked $count"
+        }
+
+
+
+        handler.postDelayed(object : Runnable {
+            override fun run() {
+                count++
+                val randomX = Random.nextInt(0, 1200) // Set your desired range
+                val randomY = Random.nextInt(0, 1920) // Set your desired range
+                findClosestButton(randomX,randomY)
+                handler.postDelayed(this, 3000)
+            }
+        }, 3000)
+
+    }
+    private fun findClosestButton(randomX: Int, randomY: Int) {
+        Log.d("MainActivity", "randonX :  $randomX, randomY : $randomY")
+        val location1 = IntArray(2)
+        button1.getLocationOnScreen(location1)
+        val distanceToButton1 = calculateDistance(randomX, randomY, location1[0], location1[1])
+
+        val location2 = IntArray(2)
+        button2.getLocationOnScreen(location2)
+        val distanceToButton2 = calculateDistance(randomX, randomY, location2[0], location2[1])
+
+        if (distanceToButton1 < distanceToButton2) {
+            simulateTouchEvent(location1[0], location1[1])
+        } else {
+            simulateTouchEvent(location2[0], location2[1])
         }
     }
-
+    private fun calculateDistance(x1: Int, y1: Int, x2: Int, y2: Int): Float {
+        return hypot((x2 - x1).toDouble(), (y2 - y1).toDouble()).toFloat()
+    }
     private fun simulateTouchEvent(x: Int, y: Int) {
         val motionEvent = MotionEvent.obtain(
             0,
