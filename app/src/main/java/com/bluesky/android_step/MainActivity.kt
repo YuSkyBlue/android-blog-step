cc2f87886f017ccfb97220b0ad67edca580b792e
BlueSky <dbcjdrb1997@gmail.com>
2024-02-04 20:21:47 +0900

    [Network State] Sample code init

--- app/src/main/java/com/bluesky/android_step/MainActivity.kt
+++ app/src/main/java/com/bluesky/android_step/MainActivity.kt
@@ -1,21 +1,25 @@
 package com.bluesky.android_step
 
 import android.content.BroadcastReceiver
+import android.content.Context
 import android.content.Intent
 import android.content.IntentFilter
+import android.net.ConnectivityManager
+import android.net.NetworkInfo
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.preference.PreferenceManager
 import com.bluesky.android_step.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
-    private val mReceiver : BroadcastReceiver = MyReceiver()
-
+//    private val mReceiver : BroadcastReceiver = MyReceiver()
+    private lateinit var mReceiver: MyReceiver
     private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
+        mReceiver = MyReceiver()
         binding.sendBroadcast.setOnClickListener {
             /** Custom BroadCast*/
             val intent = Intent(MyReceiver().MY_ACTION)
@@ -23,17 +27,63 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
+    override fun onStart() {
+        super.onStart()
+        val sharedPrefs = PreferenceManager.getDefaultSharedPreferences(this)
+
+        sPref = sharedPrefs.getString("listPref", "Wi-Fi")
+        updateConnectedFlags()
+        if (refreshDisplay) {
+            loadPage()
+        }
+    }
+    fun loadPage() {
+//        if (sPref == ANY && (wifiConnected || mobileConnected) || sPref == WIFI && wifiConnected) {
+//            // AsyncTask subclass
+//            DownloadXmlTask().execute(URL)
+//        } else {
+//            showErrorPage()
+//        }
+    }
+    private fun updateConnectedFlags() {
+        val connMgr = getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+
+        val activeInfo: NetworkInfo? = connMgr.activeNetworkInfo
+        if (activeInfo?.isConnected == true) {
+            wifiConnected = activeInfo.type == ConnectivityManager.TYPE_WIFI
+            mobileConnected = activeInfo.type == ConnectivityManager.TYPE_MOBILE
+        } else {
+            wifiConnected = false
+            mobileConnected = false
+        }
+    }
     override fun onResume() {
         super.onResume()
         val filter = IntentFilter()
-        filter.addAction(Intent.ACTION_POWER_CONNECTED)
-        filter.addAction(Intent.ACTION_POWER_DISCONNECTED)
+        filter.addAction(ConnectivityManager.CONNECTIVITY_ACTION)
         filter.addAction(MyReceiver().MY_ACTION)
         registerReceiver(mReceiver, filter)
     }
 
-    override fun onPause() {
-        super.onPause()
+    override fun onDestroy() {
+        super.onDestroy()
         unregisterReceiver(mReceiver)
+    }
+    companion object {
+        const val WIFI = "Wi-Fi"
+        const val ANY = "Any"
+        const val SO_URL = "http://stackoverflow.com/feeds/tag?tagnames=android&sort;=newest"
+
+        // Whether there is a Wi-Fi connection.
+        private var wifiConnected = false
+
+        // Whether there is a mobile connection.
+        private var mobileConnected = false
+
+        // Whether the display should be refreshed.
+        var refreshDisplay = true
+
+        // The user's current network preference setting.
+        var sPref: String? = null
     }
 }
--- app/src/main/java/com/bluesky/android_step/MyReceiver.kt
+++ app/src/main/java/com/bluesky/android_step/MyReceiver.kt
@@ -3,23 +3,54 @@ package com.bluesky.android_step
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
+import android.net.ConnectivityManager
+import android.net.NetworkInfo
 import android.widget.Toast
+import com.bluesky.android_step.MainActivity.Companion.ANY
+import com.bluesky.android_step.MainActivity.Companion.WIFI
+import com.bluesky.android_step.MainActivity.Companion.refreshDisplay
+import com.bluesky.android_step.MainActivity.Companion.sPref
 
 class MyReceiver : BroadcastReceiver() {
 
     /** 패키지명.action.액션명*/
-     val MY_ACTION = "com.bluesky.android_step.ACTION_MY_BROADCAST"
+    val MY_ACTION = "com.bluesky.android_step.ACTION_MY_BROADCAST"
+
+
+
     override fun onReceive(context: Context?, intent: Intent?) {
-        when (intent?.action) {
-            Intent.ACTION_POWER_CONNECTED -> {
-                Toast.makeText(context, "Power Connected", Toast.LENGTH_SHORT).show()
-            }
-            Intent.ACTION_POWER_DISCONNECTED -> {
-                Toast.makeText(context, "Power Disconnected", Toast.LENGTH_SHORT).show()
+        val conn = context?.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+        val networkInfo: NetworkInfo? = conn.activeNetworkInfo
+
+        when(intent?.action){
+            ConnectivityManager.CONNECTIVITY_ACTION -> {
+                if (WIFI == sPref && networkInfo?.type == ConnectivityManager.TYPE_WIFI) {
+                    // If device has its Wi-Fi connection, sets refreshDisplay
+                    // to true. This causes the display to be refreshed when the user
+                    // returns to the app.
+                    refreshDisplay = true
+                    Toast.makeText(context, "Wifi is Connected", Toast.LENGTH_SHORT).show()
+
+                    // If the setting is ANY network and there is a network connection
+                    // (which by process of elimination would be mobile), sets refreshDisplay to true.
+                } else if (ANY == sPref && networkInfo != null) {
+                    refreshDisplay = true
+
+                    // Otherwise, the app can't download content--either because there is no network
+                    // connection (mobile or Wi-Fi), or because the pref setting is WIFI, and there
+                    // is no Wi-Fi connection.
+                    // Sets refreshDisplay to false.
+                } else {
+                    refreshDisplay = false
+                    Toast.makeText(context, "Wifi is Disconnected", Toast.LENGTH_SHORT).show()
+                }
             }
             MY_ACTION -> {
                 Toast.makeText(context, "Custom Broadcast Received", Toast.LENGTH_SHORT).show()
             }
         }
+
+
+
     }
 }
