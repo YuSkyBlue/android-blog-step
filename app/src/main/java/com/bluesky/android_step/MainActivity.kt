c5aaca547c4499cf55f85da9afbbcf306aa134e9
bluesky <dbcjdrb1997@gmail.com>
2024-02-26 10:15:23 +0900

    [Hilt] NotUsing Hilt Version

--- app/src/main/java/com/bluesky/android_step/MainActivity.kt
+++ app/src/main/java/com/bluesky/android_step/MainActivity.kt
@@ -4,8 +4,12 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var userRepository: UserRepository
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val application = application as MyApplication
+        userRepository  = application.userRepository
     }
 }
